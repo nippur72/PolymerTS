48261401157dfc4be89d490f8ad2f2004b284f38
Antonino Porcino <nino.porcino@gmail.com>
2016-03-12 10:11:14 +0100

    build process as npm script in package.json

--- ugly.bat
+++
@@ -1,8 +0,0 @@
-rem use uglifyjs to minify JavaScript files 
-
-call uglifyjs -o polymer-ts.min.js --in-source-map polymer-ts.js.map --source-map polymer-ts.min.js.map --screw-ie8 --mangle --compress --keep-names polymer-ts.js
-
-copy polymer-ts.min.* Sample\bower_components\polymer-ts
-copy polymer-ts.min.* Test\bower_components\polymer-ts
-
-		  
